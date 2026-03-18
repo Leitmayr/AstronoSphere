f1132d7e43a278f6d2426a7cb19ae425ce616929
Leitmayr <marcus.hiemer@web.de>
2026-03-18 22:53:38 +0100

    UPDATE: TruthFactory (mesh + time user components, clean state)

---
+++ 03_TruthFactory/EphemerisRegression/Mesh/EpochDefinition.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace EphemerisRegression.Mesh
+{
+    public enum MeshEpochType
+    {
+        Core,
+        Extended,
+        Extreme
+    }
+
+    public sealed class EpochDefinition
+    {
+        public MeshEpochType EpochType { get; }
+        public MeshUtc StartUtc { get; }
+        public MeshUtc StopUtc { get; }
+        public TimeSpan InnerStep { get; }
+        public TimeSpan OuterStep { get; }
+
+        public EpochDefinition(
+            MeshEpochType epochType,
+            MeshUtc startUtc,
+            MeshUtc stopUtc,
+            TimeSpan innerStep,
+            TimeSpan outerStep)
+        {
+            EpochType = epochType;
+            StartUtc = startUtc;
+            StopUtc = stopUtc;
+            InnerStep = innerStep;
+            OuterStep = outerStep;
+        }
+
+        public static EpochDefinition CreateCore()
+        {
+            return new EpochDefinition(
+                MeshEpochType.Core,
+                new MeshUtc(1600, 1, 1),
+                new MeshUtc(2400, 12, 31),
+                TimeSpan.FromDays(30),
+                TimeSpan.FromDays(60));
+        }
+
+        public static EpochDefinition CreateExtended()
+        {
+            return new EpochDefinition(
+                MeshEpochType.Extended,
+                new MeshUtc(0, 1, 1),
+                new MeshUtc(4000, 12, 31),
+                TimeSpan.FromDays(180),
+                TimeSpan.FromDays(180));
+        }
+
+        public static EpochDefinition CreateExtreme()
+        {
+            return new EpochDefinition(
+                MeshEpochType.Extreme,
+                new MeshUtc(-4000, 1, 1),
+                new MeshUtc(8000, 12, 31),
+                TimeSpan.FromDays(365 * 2),
+                TimeSpan.FromDays(365 * 2));
+        }
+    }
+}
---
+++ 04_Astronometria/src/AstroSim.Time.User/UTCInstant.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AstroSim.Time.User
+{
+    public readonly struct UTCInstant
+    {
+        public int Year { get; }
+        public int Month { get; }
+        public int Day { get; }
+        public int Hour { get; }
+        public int Minute { get; }
+        public int Second { get; }
+
+        public UTCInstant(int year, int month, int day,
+                          int hour, int minute, int second)
+        {
+            Year = year;
+            Month = month;
+            Day = day;
+            Hour = hour;
+            Minute = minute;
+            Second = second;
+        }
+
+        public override string ToString()
+            => $"{Year:D4}-{Month:D2}-{Day:D2} {Hour:D2}:{Minute:D2}:{Second:D2} UTC";
+    }
+}
